e5e5b47b50a9ab79ced972c4885ca700619d84af
majdhallak <majdhallak90@gmail.com>
2022-08-09 14:56:05 +0300

    updated package.json

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,23 +1,21 @@
 import React from 'react'
-import { SafeAreaView } from 'react-native'
+
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../Sidebar'
 import './index.scss'
 
 const Layout = () => {
   return (
-    <SafeAreaView>
-      <div className="App">
-        <Sidebar />
-        <div className="page">
-          <span className="tags top-tags">{/* &lt;body&gt; */}</span>
-          <Outlet />
-          <span className="tags bottom-tags">{/* &lt;body&gt; */}</span>
-          <br />
-          <span className="bottom-tag-html">{/* &lt;/html&gt; */}</span>
-        </div>
+    <div className="App">
+      <Sidebar />
+      <div className="page">
+        <span className="tags top-tags">{/* &lt;body&gt; */}</span>
+        <Outlet />
+        <span className="tags bottom-tags">{/* &lt;body&gt; */}</span>
+        <br />
+        <span className="bottom-tag-html">{/* &lt;/html&gt; */}</span>
       </div>
-    </SafeAreaView>
+    </div>
   )
 }
 
